48b7842cb625c3898ccaa5770da5f5ad70084e77
Albert <greatte@mail.ru>
2023-10-17 19:49:31 +0700

    Add typescript sdk

---
+++ typescript-fetch/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ChannelApi';
+export * from './MediaApi';
+export * from './MessagingApi';
+export * from './ProductsCatalogsApi';
+export * from './TemplatesApi';
+export * from './WebhooksApi';
